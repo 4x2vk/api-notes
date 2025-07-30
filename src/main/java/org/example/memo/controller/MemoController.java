e2afa762d34b68ba8c1286bfbbfbe5e833ea5e96
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-07-30 18:35:32 +0900

    [feat] implement updateTitle function

--- src/main/java/org/example/memo/controller/MemoController.java
+++ src/main/java/org/example/memo/controller/MemoController.java
@@ -60,6 +60,23 @@ public class MemoController {
 
         return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ) {
+        Memo memo = memoList.get(id);
+        if (null == memo) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (dto.getTitle() == null || dto.getContent() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
     @DeleteMapping("/{id}")
     public void deleteMemoById(@PathVariable Long id) {
         memoList.remove(id);
